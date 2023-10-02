cd66645f5e627d7fe76281a3b575c0c712d726f9
Yzeghal <yanis.zeghal@ensae.fr>
2023-10-02 22:07:54 +0200

    Update Bias_simulation.R
    Cleaned a lot

--- Bias_simulation.R
+++ Bias_simulation.R
@@ -27,7 +27,7 @@ R2 = function(m){
 ###################################################
 # Regression of centered variables without constant.
 set.seed(18)
-V = diag(3) #variance matrix : base will be orthogonal and normed vectors.
+V = diag(3) #variance matrix : basis will be orthogonal and normed vectors.
 X = mvrnorm(n=50000, mu = c(0,0,0), Sigma = V)
 
 x1 = X[,1]
@@ -67,30 +67,30 @@ reg_constant
 ###########################
 # Illustrating the use of a constant in 3D
 
-base <- diag(3)
-rownames(base) <- c("x1", "x2", "1")
+basis <- diag(3)
+rownames(basis) <- c("x1", "x2", "1")
 
-vec = matrix(1/sqrt(2)*base[1,]+sqrt(1-1/sqrt(2)^2)*base[2,], nrow = 1)
+vec = matrix(1/sqrt(2)*basis[1,]+sqrt(1-1/sqrt(2)^2)*basis[2,], nrow = 1)
 rownames(vec) = "Y"
 open3d()
 planes3d(0, 0, 1, 0, col="gray", alpha=0.2)
 
-vectors3d(base, col = "black")
+vectors3d(basis, col = "black")
 vectors3d(vec, col = "red")
 segments3d(rbind(c(1/sqrt(2),0,0),c(1/sqrt(2),1/sqrt(2),0)))
 segments3d(rbind(c(0,1/sqrt(2),0),c(1/sqrt(2),1/sqrt(2),0)))
-# close3d()
+close3d()
 open3d()
 
-names(base) = NULL
-vecX1O = matrix(base[1,]+1*base[3,], nrow = 1)
-vecX2O = matrix(base[2,]+3*base[3,], nrow = 1)
-vecY = matrix(1/sqrt(2)*base[1,]+1/sqrt(2)*base[2,]+4*base[3,], nrow = 1)
+names(basis) = NULL
+vecX1O = matrix(basis[1,]+1*basis[3,], nrow = 1)
+vecX2O = matrix(basis[2,]+3*basis[3,], nrow = 1)
+vecY = matrix(1/sqrt(2)*basis[1,]+1/sqrt(2)*basis[2,]+4*basis[3,], nrow = 1)
 vec = rbind(vecX1O, vecX2O, vecY)
 rownames(vec) = c("x1o", "x2o", "yo")
 
 planes3d(0, 0, 1, 0, col="gray", alpha=0.2)
-vectors3d(base, col = "black")
+vectors3d(basis, col = "black")
 vectors3d(vec, col = "blue")
 segments3d(rbind(c(1/sqrt(2),0,0),c(1/sqrt(2),1/sqrt(2),0)))
 segments3d(rbind(c(0,1/sqrt(2),0),c(1/sqrt(2),1/sqrt(2),0)))
@@ -100,15 +100,15 @@ segments3d(rbind(c(1/sqrt(2),1/sqrt(2),0),c(1/sqrt(2),1/sqrt(2),4)))
 
 open3d()
 
-names(base) = NULL
-vecX1O = matrix(base[1,]+2*base[3,], nrow = 1)
-vecX2O = matrix(base[2,]+6*base[3,], nrow = 1)
-vecY = matrix(1/sqrt(2)*base[1,]+1/sqrt(2)*base[2,]+8*base[3,], nrow = 1)
+names(basis) = NULL
+vecX1O = matrix(basis[1,]+2*basis[3,], nrow = 1)
+vecX2O = matrix(basis[2,]+6*basis[3,], nrow = 1)
+vecY = matrix(1/sqrt(2)*basis[1,]+1/sqrt(2)*basis[2,]+8*basis[3,], nrow = 1)
 vec = rbind(vecX1O, vecX2O, vecY)
 rownames(vec) = c("x1o", "x2o", "yo")
 
 planes3d(0, 0, 1, 0, col="gray", alpha=0.2)
-vectors3d(base, col = "black")
+vectors3d(basis, col = "black")
 vectors3d(vec, col = "green")
 segments3d(rbind(c(1/sqrt(2),0,0),c(1/sqrt(2),1/sqrt(2),0)))
 segments3d(rbind(c(0,1/sqrt(2),0),c(1/sqrt(2),1/sqrt(2),0)))
@@ -126,9 +126,9 @@ close3d()
 
 #####
 vec <- diag(3)
-rownames(vec) <- c("e1", "e2", "e3")
+rownames(vec) <- c("E1", "E2", "E3")
 open3d()
-vectors3d(vec, color=rep("black",3), lwd=2, cex.lab =2)
+vectors3d(vec, color=rep("black",3), lwd=2, frac.lab =1.2, cex.lab = 2)
 # draw the XZ plane, whose equation is Y=0
 planes3d(0, 0, 1, 0, col="gray", alpha=0.2)
 
@@ -137,16 +137,16 @@ planes3d(0, 0, 1, 0, col="gray", alpha=0.2)
 # First regression 
 set.seed(18)
 V = diag(3) # variance matrix 
-E = mvrnorm(n=50000, mu = c(0,0,0), Sigma = V)
+e = mvrnorm(n=50000, mu = c(0,0,0), Sigma = V)
 
-e1 = E[,1]
-e2 = E[,2]
-e3 = E[,3]
+e1 = e[,1]
+e2 = e[,2]
+e3 = e[,3]
 
-# e1,e2,e3 are observations of 3  random variables that form an orthonormal base of a 3 dimension subspace of RV in |R
+# e1,e2,e3 are observations of 3  random variables that form an orthonormal basis
+# of a 3 dimension subspace of RV in |R
 
 # 1st step : generating y
-
 y = e3+e2
 reg1 = lm(y~e2-1) # -1 specifies not to add a constant to the model.
 reg1
@@ -154,15 +154,18 @@ R2(reg1)          # ~0.5 as expected : ||e2||^2/||e2+e3||^2
 
 # 3D representation of reg1
 
-base <- diag(3)
-rownames(base) <- c("e1", "e2", "e3")
+basis <- diag(3)
+rownames(basis) <- c("E1", "E2", "E3")
 
+Y = matrix(basis[2,]+ basis[3,], nrow = 1)
+rownames(Y) = "Y"
+E2 = matrix(basis[2,], nrow = 1)
+rownames(E2) = "E2"
 open3d()
 planes3d(0, 0, 1, 0, col="lightgrey")
-
-vectors3d(base[c(1,3),], col = "black",  lwd = 2)
-vectors3d(c(0,1,1), col ="red",  lwd = 2, labels = "Y")
-vectors3d(base[2,], col = "blue4",  lwd = 2, labels ="e2")
+vectors3d(basis[c(1,3),], col = "black",  lwd = 2)
+vectors3d(Y, col ="red",  lwd = 2, frac.lab = 1.05)
+vectors3d(E2, col = "blue4",  lwd = 2, frac.lab = 1.2)
 segments3d(rbind(c(0,1,0),c(0,1,1)), col = "red")
 corner(c(0,0,0),c(0,1,0),c(0,1,1), col = "red")
 light3d(0, 0)
@@ -182,18 +185,18 @@ R2(reg2)
 
 
 # 3D representation of reg2
-base <- diag(3)
-rownames(base) <- c("e1", "e2", "e3")
-vec = matrix(1/sqrt(2)*base[1,]+1/sqrt(2)*base[2,], nrow = 1)
+basis <- diag(3)
+rownames(basis) <- c("E1", "E2", "E3")
+X1 = matrix(1/sqrt(2)*basis[1,]+1/sqrt(2)*basis[2,], nrow = 1)
+rownames(X1) = "X1"
 
-rownames(vec) = "X1"
 open3d()
 planes3d(0, 0, 1, 0, col="cornflowerblue")
-
-vectors3d(base[c(1,3),], col = "black",  lwd = 2)
-vectors3d(c(0,1,1), col ="red",  lwd = 2, labels = "Y")
-vectors3d(base[2,], col = "blue4",  lwd = 2, labels ="e2")
-vectors3d(vec, col = "blue4", lwd = 2)
+ 
+vectors3d(basis[c(1,3),], col = "black",  lwd = 2)
+vectors3d(Y, col ="red",  lwd = 2, frac.lab = 1.05)
+vectors3d(E2, col = "blue4",  lwd = 2, frac.lab = 1.2)
+vectors3d(X1, col = "blue4", lwd = 2)
 segments3d(rbind(c(0,1,0),c(0,1,1)), col = "red")
 corner(c(0,0,0),c(0,1,0),c(0,1,1), col = "red")
 
@@ -201,8 +204,30 @@ light3d(0, 0)
 light3d(0, 0)
 light3d(0, 0)
 close3d()
+# 3D representation of Reg2 if  coefficient of X1 is not 0:
+basis <- diag(3)
+rownames(basis) <- c("E1", "E2", "E3")
+X1offset =as.matrix(X1/3+E2, nrow = 1)
+rownames(X1offset) = "X1"
 
-# 3rd step : A control orthogonal to e2 cannnot change its coefficient.
+open3d()
+planes3d(0, 0, 1, 0, col="cornflowerblue")
+
+vectors3d(basis[c(1,3),], col = "black",  lwd = 2)
+vectors3d(Y, col ="red",  lwd = 2, frac.lab = 1.05)
+vectors3d(E2, col = "blue4",  lwd = 2, frac.lab = 1.2)
+vectors3d(X1offset, col = "blue4", lwd = 2, origin = E2, frac.lab = 0.5)
+segments3d(rbind(c(0,1,0),c(0,1,1)), col = "red", lwd = 2) # eta
+corner(c(0,0,0),c(0,1,0),c(0,1,1), col = "red")
+segments3d(rbind(X1offset,c(0,1,1)), col = "darkorange2", lwd = 2) #eta'
+
+light3d(0, 0)
+light3d(0, 0)
+light3d(0, 0)
+close3d()
+
+
+# 3rd step : A control orthogonal to e2 cannot change its coefficient.
 
 xo = 1/sqrt(2)*e1+1/sqrt(2)*e3
 
@@ -210,17 +235,17 @@ cor(xo,e2)  # "Almost" 0
 
 reg3 = lm(y~e2+xo-1)
 reg3
-
 R2(reg3)
-vec =rbind( 1/sqrt(2)*c(1,0,1), c(0,1,0)) %*% base
-rownames(vec) = c("Xo", "e2")
+# 3D representation of reg3
+vec =rbind( 1/sqrt(2)*c(1,0,1), c(0,1,0)) %*% basis
+rownames(vec) = c("Xo", "E2")
 pointfall = t(vec)%*%project(t(vec))%*% matrix(c(0,1,1), nrow = 3)
 open3d()
 planes3d(0, 0, 1, 0, col="lightgrey")
 planes3d(1, 0,-1, 0, col = "cornflowerblue")
-vectors3d(base[c(1,3),], col = "black",  lwd = 2)
+vectors3d(basis[c(1,3),], col = "black",  lwd = 2)
 vectors3d(vec, col = "blue4", lwd = 2)
-vectors3d(c(0,1,1),label = "Y", col = "red", lwd = 2)
+vectors3d(Y, col = "red", lwd = 2, frac.lab = 1.05)
 segments3d(rbind(c(0,0,0),t(pointfall)), col = "red")
 segments3d(rbind(t(pointfall),c(0,1,1)), col = "red")
 segments3d(rbind(t(pointfall),c(0,1,0)), col = "black")
@@ -232,26 +257,49 @@ light3d(0,0)
 
 open3d()
 planes3d(0, 0, 1, 0, col="lightgrey")
-vectors3d(base[c(1,3),], col = "black",  lwd = 2)
+
+vectors3d(basis[c(1,3),], col = "black",  lwd = 2)
 vectors3d(vec, col = "blue4", lwd = 2)
-vectors3d(c(0,1,1),label = "Y", col = "red", lwd = 2)
-segments3d(rbind(c(0,1,0),c(0,1,1)), col = "red")
-corner(c(0,0,0),c(0,1,0),c(0,1,1), col = "red")
-light3d(0, 0)
-light3d(0, 0)
+vectors3d(Y, col = "red", lwd = 2, frac.lab = 1.05)
+
+corner(c(0,0,0),c(0,1,0),as.vector(Y), col = "red")
+segments3d(rbind(c(0,1,0), as.vector(Y)), col = "red")
+
+light3d(0,0)
+light3d(0,0)
 
 close3d()
 close3d()
 
 
-# 3rd step : adding a control in vect(e2, eta) may induce bias
+# 4th step : adding a control in vect(e2, eta) induces bias
 xc = e3+1/2*e2
 reg3 = lm(y~e2+xc-1)
 reg3
 R2(reg3)
+# 3D representation of extra control
+eta = matrix(c(0,0,1), nrow = 1)
+rownames(eta) = "eta"
+D = matrix(c(0,1,0), nrow = 1)
+rownames(D) = "D"
+X = matrix(c(1/sqrt(3),1/sqrt(3),1/sqrt(3),-1/sqrt(3),1/sqrt(3),1/sqrt(3)), nrow = 2, byrow = TRUE)
+rownames(X) = c("X1","X2")
+Xc = matrix(c(0,1/sqrt(2), 1/sqrt(2)), nrow = 1)
+rownames(Xc) = "Xc"
 
+open3d()
+vectors3d(D, lwd = 3, frac.lab = 1.1, col = "dodgerblue")
+vectors3d(X, lwd = 3, frac.lab = 0.5, col = "black")
+vectors3d(eta, lwd = 3, frac.lab = 0.5, col = "red")
+vectors3d(Xc, lwd = 3, frac.lab = 0.8, col = "green3")
+planes3d(0,0,1,0, col = "lightgrey")
+planes3d(0,1,-1,0, col = "lemonchiffon", alpha = 0.6)
+text3d(0,0.6,0.8, "Vect(X1,X2)", col = "lemonchiffon4", font = 2)
+light3d(0,0)
+
+close3d()
 # 3D representation of reg4
-vec =rbind(c(0,0.5,1), c(0,1,0)) %*% base
+vec =rbind(c(0,0.5,1), c(0,1,0)) %*% basis
 rownames(vec) = c("Xc", "e2")
 
 userMat = matrix(c(0,1,0,0,0,0,1,0,1,0,0,0,0,0,0,1),byrow = TRUE, nrow = 4) # view from front
@@ -260,9 +308,9 @@ open3d()
 view3d(userMatrix = userMat2)
 planes3d(0, 0, 1, 0, col="lightgrey")
 planes3d(1, 0, 0, 0, col = "cornflowerblue")
-vectors3d(base[c(1,3),], col = "black",  lwd = 2)
-vectors3d(vec, col = "blue4", lwd = 2)
-vectors3d(c(0,1,1),label = "Y", col = "red", lwd = 2)
+vectors3d(basis[c(1,3),], col = "black",  lwd = 2)
+vectors3d(vec, col = "blue4", lwd = 2, frac.lab = 1.1)
+vectors3d(Y, col = "red", lwd = 2)
 
 
 # more cases of reg4
@@ -274,7 +322,7 @@ for (i in 1:6){
   vecxc =matrix(c(0,0.3*i,1),nrow= 1)
   
   rownames(vecxc) = "Xc"
-  vectors3d(base,lwd=2)
+  vectors3d(basis,lwd=2)
   segments3d(rbind(c(0,1,1),  c(0.0001,0.3*i,1)), col= "blue4", lwd=2)
   if(i<=1/0.3){
     cone3d(tip = c(0.001,0.1,0), base= c(0,0.9,1) ,length = 0.05, radius= 0.02, col = "blue4" )
@@ -284,13 +332,13 @@ for (i in 1:6){
     cone3d(tip = c(0.001,-0.1,0), base= c(0,1.1,1),length = 0.05, radius= 0.02, col = "blue4")
 
   }
-  vectors3d(vecxc, col = "blue4",lwd=2)
-  vectors3d(c(0,1,1),label = "Y", col = "red", lwd = 2)
+  vectors3d(vecxc, col = "blue4",lwd=2, frac.lab = 0.5)
+  vectors3d(Y, col = "red", lwd = 2)
   planes3d(1, 0, 0, 0, col = "cornflowerblue")
   planes3d(0,0,1,0, col = "lightgrey")
   view3d(userMatrix = userMat2)
 
-  filename = paste("C:/Users/tayoy/Documents/ENSAE/RBB - Regressions/reg4.",i,".png", sep = "")
+  filename = paste("C:/Users/tayoy/Documents/ENSAE/2A/RBB - Regressions/reg4.",i,".png", sep = "")
   rgl.snapshot(filename, fmt = 'png')
 }
 
@@ -302,42 +350,31 @@ close3d()
 close3d()
 close3d()
 
+# Regression 4.7 if beta_x is bounded
 open3d()
-
 vecxc = matrix(c(0,1 ,05/18),nrow= 1)
 vece2 = matrix(c(0,0.5,0),nrow= 1)
-veceps= matrix(c(0,1,1), nrow = 1)
+veceta= matrix(c(0,1,1), nrow = 1)
 vecy = matrix(c(0,1,1), nrow = 1)
 rownames(vecy) = "Y"
 rownames(vecxc) = "Xc"
-rownames(vece2) = "e2"
-rownames(veceps) = "eta"
-vectors3d(base[3,],lwd=2)
-vectors3d(vece2,color = "blue4", lwd = 2)
+rownames(vece2) = "E2"
+rownames(veceta) = "eta"
+vectors3d(basis[3,],lwd=2)
+vectors3d(vece2,color = "blue4", lwd = 2, frac.lab = 0.5)
 vectors3d(vecxc, origin = vece2, color = "blue4", lwd = 2,frac.lab = 0.5)
-vectors3d(veceps, origin = vecxc, color = "black", lwd = 2,frac.lab = 0.5)
+vectors3d(veceta, origin = vecxc, color = "black", lwd = 2,frac.lab = 0.5)
 vectors3d(vecy, col = "red", lwd = 2,frac.lab = 0.5)
 planes3d(1, 0, 0, 0.0001, col = "cornflowerblue")
 planes3d(0,0,1,0, col = "lightgrey")
 view3d(userMatrix = userMat)
 
-filename = paste("C:/Users/tayoy/Documents/ENSAE/RBB - Regressions/reg4.",7,".png", sep = "")
+filename = paste("C:/Users/tayoy/Documents/ENSAE/2A/RBB - Regressions/reg4.",7,".png", sep = "")
 rgl.snapshot(filename, fmt = 'png')
 close3d()
 
 
-open3d()
-planes3d(0, 0, 1, 0, col="lightgrey")
-planes3d(1, 0, 0, 0, col = "cornflowerblue")
-vectors3d(base[c(1,3),], col = "black",  lwd = 2)
-vectors3d(vec, col = "blue4", lwd = 2)
-vectors3d(c(0,1,1),label = "Y", col = "red", lwd = 2)
 
-segments3d(rbind(c(0,0,0),t(pointfall)), col = "red")
-segments3d(rbind(t(pointfall),c(0,1,1)), col = "red")
-segments3d(rbind(t(pointfall),c(0,1,0)), col = "black")
-corner(c(0,0,0),as.vector(pointfall),c(0,1,1), col = "red")
-corner(c(0,0,0),c(0,1,0),as.vector(pointfall), col = "black")
 
 ######
 #Second part : mixed cases
@@ -358,7 +395,7 @@ R2(reg5)
 
 #Theoretical counterpart :
 A = matrix( c(1/sqrt(3),1/sqrt(3),1/sqrt(3),0,1,0), nrow = 3) 
-M = matrix.inverse(t(A)%*%A)%*%t(A)  # Vector-> coordinates in (x1e,e2) base.
+M = matrix.inverse(t(A)%*%A)%*%t(A)  # Vector-> coordinates in (x1e,e2) basis.
 gamma_0 = M%*%c(0,1,1) #theoretical coefficients
 rownames(gamma_0) = c("X1e", "e2")
 gamma_0
@@ -398,7 +435,7 @@ summary(reg5.2)
 
 # More realistic example :
 
-V2 = matrix(rep(1,9), nrow = 3) #variance matrix : base will be orthogonal and normed vectors.
+V2 = matrix(rep(1,9), nrow = 3) #variance matrix : basis will be orthogonal and normed vectors.
 V2 = V2+diag(3)
 V2[c(3,7)] = 0
 V2=V2/2
